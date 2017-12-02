f73b7bac44050bf90cf20b5868dc4d0356baca8b
garrettlew <garrett.r.lew@gmail.com>
2017-12-02 13:53:12 -0800

    Add files via upload
    Added WifiAP server code
    
    Changed alarmControl and getValue

--- Flame_On_v1.0.ino
+++ Flame_On_v1.0.ino
@@ -8,6 +8,14 @@
  * 11/30/17
  */
 
+/*
+Wiring:
+1.VCC to 5V on Arduino board
+2.  GND to GND on Arduino board
+3.  REC to Pin 11 on Arduino board
+4.  P-E to Pin 13 on Arduino board
+*/
+
 #include <CPE123_Fall17.h>
 #include "CPE123_EspAPI_Fall17.h"
 
@@ -24,6 +32,12 @@ const int ledPin44 = 44;
 const int ledPin46 = 46;
 const int ledPin48 = 48;
 
+// Pins - also assumes using Serial1 to talk with ESP board
+const int espResetPin = 53;
+const int boardLedPin = 42;
+const int connectedLedPin = 40;
+const int tcpLedPin = 38;
+
 //Hardware
 Led ledOne(ledPin44);
 
@@ -31,6 +45,20 @@ Button overideButton(buttonPin);
 
 Led bluLed(ledPin46);
 Led redLed(ledPin48);
+
+Led boardLed(boardLedPin);
+Led connectedLed(connectedLedPin);
+Led tcpLed(tcpLedPin);
+
+// Used to setup the ESP as an Access Point.
+const char apSSID[] = "FlameOn";
+const char apWifiPassword[] = "fourwordsalluppercase";
+
+// Used to setup the ESP as a telnet server (port 23)
+const unsigned int tcpServerPort = 23; // telnet
+
+// Used to communicate between the Mega and the ESP device
+HardwareSerial & espSerial = Serial3;
 
 void setup() 
 {
@@ -45,6 +73,16 @@ void setup()
 
   bluLed.off();
   redLed.off();
+
+  // setup esp, AP and telnet server
+  setupEsp_AP_TelnetServer();
+  
+  //  Put esp into serial mode (mega send/recv over network use espSerial)
+  setupSerialTextMode();
+  
+  // just to make things faster - makes waiting on esp/Serial monitor input faster
+  espSerial.setTimeout(500);
+  Serial.setTimeout(10);
 }
 
 void loop() 
@@ -219,12 +257,10 @@ int getValue()
 {
   Serial.setTimeout(10);
   int value = 0;
-  while (Serial.available() <= 0)
+  if (espSerial.available() > 0)
   {
-    //Wait
+    value = espSerial.parseInt();
   }
-
-  value = Serial.parseInt();
 
   return value;
 }
@@ -232,23 +268,23 @@ int getValue()
 void alarmControl()
 {
   Serial.setTimeout(10);
-  int shouldAlarm = 0;
+  int shouldChangeState = 0;
   enum {WAITING, ALARMING};
   static int state = ALARMING;
 
-  shouldAlarm = getValue();
+  shouldChangeState = getValue();
 
   switch (state)
   {
     case WAITING:
-      if (shouldAlarm > 0)
+      if (shouldChangeState > 0)
       {
         state = ALARMING;
       }
     break;
 
     case ALARMING:
-      if (shouldAlarm <= 0)
+      if (shouldChangeState > 0)
       {
         playAlert(false);
         blinkLeds(false);
@@ -264,6 +300,27 @@ void alarmControl()
       Serial.println("error");
     break;
   }
+}
+
+void setupEsp_AP_TelnetServer()
+{
+  espSerial.begin(9600);
+  
+  // configure the Esp board
+  setupEspLeds(boardLed, connectedLed, tcpLed, espResetPin);
+  espBoardReset("Rebooting ESP board via reset pin"); 
+    
+  // Output ESP Firmware version (helps us verify the boards is working)
+  reportEspFirmwareVersion();
+    
+  // Set up Esp in Access Point mode 
+  setupWifiApMode(apSSID, apWifiPassword);
+  print4("WiFi AP Mode setup complete SSID: ", apSSID, " Password: ", apWifiPassword);
+  
+  // Now setup the TCP server and wait (block) for a client
+  tcpServerSetup(tcpServerPort, "Prof. Smith's Access Point/Telnet test");
+  tcpServerWaitForClient();
+  
 }
 
 
